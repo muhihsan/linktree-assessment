8943113ec312811a9e67e969931abb712884634e
Muhammad Ihsan <mihsan191@gmail.com>
2022-04-09 13:47:07 +1000

    feat: Add db client for post link

--- src/dataSources/dbClient.test.ts
+++ src/dataSources/dbClient.test.ts
@@ -1,18 +1,59 @@
 import { v4 as uuid } from "uuid";
-import { createDbClient } from "./dbClient";
+import { Link } from "../types";
+import { createDbClient, linksDatabase } from "./dbClient";
 
 describe("dynamnoDbClient", () => {
   // TODO: create default test mock config to avoid usage of any
   const mockConfig = {} as any;
   const dbClient = createDbClient(mockConfig);
 
+  beforeEach(() => {
+    linksDatabase.length = 0;
+  });
+
   describe("getLinks", () => {
     it("should return links for user", async () => {
       const userId = uuid();
+      const expectedLink = {
+        id: uuid(),
+        userId,
+        name: "first link",
+        link: "https://testing.com",
+        createdDate: new Date().toISOString(),
+        updatedDate: new Date().toISOString(),
+      };
+
+      linksDatabase.push(expectedLink, {
+        id: uuid(),
+        userId: uuid(),
+        name: "second link",
+        link: "https://testing.com",
+        createdDate: new Date().toISOString(),
+        updatedDate: new Date().toISOString(),
+      });
 
       const results = await dbClient.getLinks(userId);
 
-      expect(results[0].userId).toEqual(userId);
+      expect(results).toEqual([expectedLink]);
+    });
+  });
+
+  describe("postLink", () => {
+    it("should return update link array", async () => {
+      const userId = uuid();
+
+      const link: Link = {
+        id: uuid(),
+        userId,
+        name: "testing",
+        link: "https://testing.com",
+        createdDate: new Date().toISOString(),
+        updatedDate: new Date().toISOString(),
+      } as Link;
+
+      await dbClient.postLink(link);
+
+      expect(linksDatabase).toContain(link);
     });
   });
 });
--- src/dataSources/dbClient.ts
+++ src/dataSources/dbClient.ts
@@ -2,23 +2,23 @@ import { v4 as uuid } from "uuid";
 import { Config, Link } from "../types";
 
 export type DataSource = (config: Config) => {
-  getLinks: (userId: string) => Promise<Array<Link>>;
+  getLinks: (userId: string) => Promise<Link[]>;
+  postLink: (link: Link) => Promise<void>;
 };
+
+export const linksDatabase: Link[] = [];
 
 export const createDbClient: DataSource = (config) => {
   return {
     getLinks: (userId: string) => {
-      // TODO: Get from actual DynamoDB
-      return Promise.resolve([
-        {
-          id: uuid(),
-          userId,
-          name: "testing",
-          link: "https://testing.com",
-          createdDate: new Date().toISOString(),
-          updatedDate: new Date().toISOString(),
-        } as Link,
-      ]);
+      // TODO: Get from actual Database
+      return Promise.resolve(
+        linksDatabase.filter((link) => link.userId === userId)
+      );
+    },
+    postLink: async (link: Link) => {
+      // TODO: store to actual Database
+      linksDatabase.push(link);
     },
   };
 };
