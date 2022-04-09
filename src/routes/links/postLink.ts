58d9902364b5ae7488a872c73d6f54eba0f6de05
Muhammad Ihsan <mihsan191@gmail.com>
2022-04-09 13:55:40 +1000

    feat: Add db client for post link

---
+++ src/routes/links/postLink.ts
@@ -0,0 +1,14 @@
+import { Context, Middleware } from "koa";
+import { State } from "../../types";
+
+export const getLinksHandler: Middleware = async (ctx: Context) => {
+  const {
+    dataSources: { dbClient },
+  } = <State>ctx.state;
+
+  const { userId } = ctx.params;
+
+  const links = await dbClient.getLinks(userId);
+
+  ctx.body = links;
+};
